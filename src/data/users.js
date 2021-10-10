bebbb6016e8f9dcff69a9ede599e53192cd780b5
JacobFeat <jakubk198@gmail.com>
2021-10-10 23:44:12 +0200

    set UserListItem dectructuring

---
+++ src/data/users.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    name: 'Andrzej Pasecki',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Jakub Kita',
+    attendance: '21%',
+    average: '3.3',
+  },
+  {
+    name: 'Adam Małysz',
+    attendance: '79%',
+    average: '4.2',
+  },
+  {
+    name: 'Tyson Fury',
+    attendance: '21%',
+    average: '2.1',
+  },
+  {
+    name: 'Anthony Joshua',
+    attendance: '99%',
+    average: '4.9',
+  },
+  {
+    name: 'Adrian Polak',
+    attendance: '11%',
+    average: '3.2',
+  },
+];
---
+++ src/views/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UserList from 'components/UsersList/UsersList';
+
+const Root = () => {
+  return (
+    <>
+      <UserList />
+    </>
+  );
+};
+
+Root.propTypes = {};
+
+export default Root;
