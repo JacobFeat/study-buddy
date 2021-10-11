fd2786b989bff9c7ed851ddf4514d0a7a101293e
JacobFeat <jakubk198@gmail.com>
2021-10-11 19:04:06 +0200

    make a button

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import UserList from 'components/UsersList/UsersList';
+import UserList from 'components/organisms/UsersList/UsersList';
+import styled from 'styled-components';
 
-const Root = () => {
-  return (
-    <>
-      <UserList />
-    </>
-  );
-};
+const Wrapper = styled.div`
+  background-color: #f7f8fa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Root = () => (
+  <Wrapper>
+    <UserList />
+  </Wrapper>
+);
 
 export default Root;
