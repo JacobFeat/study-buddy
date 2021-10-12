a2c7e7fec6eaae802142260f7f3a4cadbe2b23bd
JacobFeat <jakubk198@gmail.com>
2021-10-12 15:41:54 +0200

    thinking about getting value

--- src/components/atoms/Average/Average.js
+++ src/components/atoms/Average/Average.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { StyledButton } from '../Button/Button.styles';
 
 const StyledAverage = styled.div`
   width: 34px;
   height: 34px;
-  background-color: ${({ theme }) => {
-    return theme.colors.black;
-  }};
   color: ${({ theme }) => theme.colors.white};
   font-size: ${({ theme }) => theme.fontSize.m};
   font-weight: bold;
@@ -14,12 +12,18 @@ const StyledAverage = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: ${({ theme, value }) => {
+    // console.log(Average);
+    if (value > 4) return theme.colors.success;
+  }};
 `;
 
-const Average = ({ average }) => (
-  <StyledAverage average>
-    <div>{average}</div>
-  </StyledAverage>
-);
+const Average = ({ average }) => {
+  return (
+    <StyledAverage average>
+      <div>{average}</div>
+    </StyledAverage>
+  );
+};
 
 export default Average;
